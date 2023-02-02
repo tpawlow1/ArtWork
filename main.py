731f00f79271730f64b7514e66242422e079454e
tpawlow1 <122371550+tpawlow1@users.noreply.github.com>
2023-02-01 17:37:59 -0800

    super basic sql connection in main.py, no database set up

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "password"
+)
 
 @app.route("/")
 def hello_world():
