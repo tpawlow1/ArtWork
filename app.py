3be40f39010be7b4d3f7b1b80bdd1c5bd24ce722
vrgovindu <vivek.govindu@gmail.com>
2023-02-18 18:47:35 -0500

    Edit Profile overlay created

--- app.py
+++ app.py
@@ -4,22 +4,25 @@ import mysql.connector
 app = Flask(__name__)
 
 mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "password", 
-    database= "ArtWork"
+    host="localhost",
+    user="root",
+    password="password",
+    database="ArtWork"
 )
 
 
 mysqlcursor = mydb.cursor()
 
+
 @app.get("/")
 def index():
     return render_template('index.html')
 
+
 @app.get("/signup")
 def get_Signup():
     return render_template('signup.html')
+
 
 @app.post("/signup")
 def Signup():
@@ -29,8 +32,9 @@ def Signup():
     userpass1 = request.form.get('pass1')
     userpass2 = request.form.get('pass2')
     # verify passwords match
-    if userpass1 != userpass2: 
-        return render_template('index.html') # placeholder bounce back if no match
+    if userpass1 != userpass2:
+        # placeholder bounce back if no match
+        return render_template('index.html')
     # if all good, send to user table in database
     addcom = 'INSERT INTO Users VALUES (%s, %s, %s)'
     addvals = (username, email, userpass1)
@@ -39,9 +43,14 @@ def Signup():
 
     mydb.commit()
 
-    return render_template('index.html') # send user back to homepage or sign in
+    # send user back to homepage or sign in
+    return render_template('index.html')
+
+
+@app.get("/profile")
+def profilePage():
+    return render_template("profilePage.html")
 
 
 if __name__ == "__main__":
     app.run()
-
