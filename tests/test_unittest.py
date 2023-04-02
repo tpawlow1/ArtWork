758187c5670dc07e313c71c7ef9b10cd560a5cbf
tpawlow1 <122371550+tpawlow1@users.noreply.github.com>
2023-04-02 14:04:26 -0700

    sql works, tests work.

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,69 +1,76 @@
 from app import app, mydb
-import unittest
+from unittest import TestCase
 import uuid
 
-def test_signup(self):
-    # generate unique id for test user
-    username = "gh3ifmsdy"
-    usermail = username + "@gmail.com"
 
-    # test1 = good
-    response = app.test_client().post("/signup", data={
-                "usermail": usermail, 
-                "name": username, 
-                "pass1": "Passw0rd!", 
-                "pass2": "Passw0rd!",
+class AppTest(TestCase): 
+    def test_signup(self):
+        # generate unique id for test user
+        username = "gh3ifmsdy"
+        usermail = username + "@gmail.com"
+
+        # test1 = good
+        response = app.test_client().post("/signup", data={
+                    "usermail": usermail, 
+                    "name": username, 
+                    "pass1": "Passw0rd!", 
+                    "pass2": "Passw0rd!",
+                    })
+        
+        self.assertEqual(response.status_code, 302)
+        '''
+        # test2 = mismatch passwords
+        response = app.test_client().post("/signup", data={
+                    "usermail": usermail, 
+                    "name": username, 
+                    "pass1": "Passw0rd!", 
+                    "pass2": "Different!",
+                    }, follow_redirects=True)
+
+        # test3 = improper email
+        response = app.test_client().post("/signup", data={
+                    "usermail": username, 
+                    "name": username, 
+                    "pass1": "Passw0rd!", 
+                    "pass2": "Passw0rd!",
+                    }, follow_redirects=True)'''
+        
+        mysqlcursor = mydb.cursor(buffered=True)
+        query = f"DELETE FROM Users WHERE `username`='{username}'"
+        mysqlcursor.execute(query)
+        mydb.commit()
+
+
+    def test_editpost(self): 
+        postid = str(uuid.uuid4())
+
+        # connect to db and create post before editing
+        mysqlcursor = mydb.cursor(buffered=True)
+        addcom = "INSERT INTO Posts VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        addvals = (postid, 'testtitle', 'test_desc', 'bunny.jpg', 'sample', 0, 0)
+        
+        mysqlcursor.execute(addcom, addvals)
+        mydb.commit()
+        mysqlcursor.close()
+
+
+        # test1 = good
+        response = app.test_client().post('/edit/1', data={
+                    "title": "test_title", 
+                    "description": "test_description",
                 })
-    
-    self.assertEqual(response.status_code, 302)
-    '''
-    # test2 = mismatch passwords
-    response = app.test_client().post("/signup", data={
-                "usermail": usermail, 
-                "name": username, 
-                "pass1": "Passw0rd!", 
-                "pass2": "Different!",
-                }, follow_redirects=True)
+        
+        
+        self.assertEqual(response.status_code, 302)
+        
 
-    # test3 = improper email
-    response = app.test_client().post("/signup", data={
-                "usermail": username, 
-                "name": username, 
-                "pass1": "Passw0rd!", 
-                "pass2": "Passw0rd!",
-                }, follow_redirects=True)'''
-    
-    mysqlcursor = mydb.cursor(buffered=True)
-    query = f"DELETE FROM Users WHERE `username`='{username}'"
-    mysqlcursor.execute(query)
-    mydb.commit()
+        mysqlcursor = mydb.cursor(buffered=True)
+        
+        query = f"DELETE FROM Posts WHERE `id`='{postid}'"
+        mysqlcursor.execute(query)
+        mydb.commit()
 
+apptest =  AppTest()
 
-def test_editpost(self): 
-    postid = str(uuid.uuid4())
-
-    # connect to db and create post before editing
-    mysqlcursor = mydb.cursor(buffered=True)
-    addcom = "INSERT INTO Posts VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    addvals = (postid, 'testtitle', 'test_desc', 'bunny.jpg', 'sample', 0, 0)
-    
-    mysqlcursor.execute(addcom, addvals)
-    mydb.commit()
-    mysqlcursor.close()
-
-
-    # test1 = good
-    response = app.test_client().post('/edit/1', data={
-                "title": "test_title", 
-                "description": "test_description",
-            }, follow_redirects=True)
-    
-
-    mysqlcursor = mydb.cursor(buffered=True)
-    
-    query = f"DELETE FROM Posts WHERE `id`='{postid}'"
-    mysqlcursor.execute(query)
-    mydb.commit()
-
-test_signup()
-test_editpost()
+apptest.test_signup()
+apptest.test_editpost()
